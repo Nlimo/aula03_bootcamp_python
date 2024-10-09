273969d1e9fd9224511709678b87f27b8185e11c
Nlimo <lucaswilliamschaves@gmail.com>
2024-10-09 09:30:37 -0300

    add codigo ao exercicio 5

--- exercicios.py
+++ exercicios.py
@@ -58,16 +58,23 @@
 
 #if idade < 18 and idade < 65:
 #    print("Idade não permitida")
-#elif "@" not in email or "." not in email:
+#elif "@" not in email or ".com" not in email:
 #    print("O email não é valido")
 #else:
-#    print("Dados validos")
+#    print("Dados de usuário válidos")
 
 ### Exercício 5: Detecção de Anomalias em Dados de Transações
 # Você está trabalhando em um sistema de detecção de fraude e precisa identificar 
 # transações suspeitas. Uma transação é considerada suspeita se o valor for superior 
 # a R$ 10.000 ou se ocorrer fora do horário comercial (antes das 9h ou depois das 18h). 
 # Dada uma transação como `transacao = {'valor': 12000, 'hora': 20}`, verifique se ela é suspeita.
+
+transacao = {'valor': 12000, 'hora': 20}
+
+if transacao['valor'] > 10000 and (transacao['hora'] > 18 or transacao['hora'] < 6):
+    print('Essa é uma transação suspeita')
+else:
+    print("Transação segura")
 
 ### Exercício 6. Contagem de Palavras em Textos
 # Objetivo:** Dado um texto, contar quantas vezes cada palavra única aparece nele.
