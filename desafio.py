1bd67de5f898aa37497ab7dc67d67ce9a5b80ad8
Nlimo <lucaswilliamschaves@gmail.com>
2024-10-13 12:05:15 -0300

    att codigo com while

--- desafio.py
+++ desafio.py
@@ -1,27 +1,52 @@
 # 1) Solicita ao usuário que digite seu nome
 
-nome = input("Digite seu nome: ")
+cotniuar_nome = ""
 
-if nome.isdigit():
-    print("Valor invalido para um nome, digite outro")
-elif len(nome) == 0:
-    print("É necessário escrever um nome")
-elif nome.isspace():
-    print("Não é permitido digitar somente espaço em branco")
+while cotniuar_nome != "true":
+    nome = input("Digite seu nome: ")
+
+    if nome.isdigit():
+        print("Valor invalido para um nome, digite outro")
+    elif len(nome) == 0:
+        print("É necessário escrever um nome")
+    elif nome.isspace():
+        print("Não é permitido digitar somente espaço em branco")
+    else:
+        cotniuar_nome = "true"
 
 # 2) Solicita ao usuário que digite o valor do seu salário
 # Converte a entrada para um número de ponto flutuante
-try:
-    salario = float(input("Digite seu salário: "))
-except ValueError:
-    print("Digite um valor valido, um número")
+
+cotniuar_salario = ""
+
+while cotniuar_salario != "true":
+    try:
+        salario = float(input("Digite seu salário: "))
+        cotniuar_salario = "true"
+    except ValueError:
+        print("Digite um valor válido, um número")
+    
+        
+
 # 3) Solicita ao usuário que digite o valor do bônus recebido
 # Converte a entrada para um número de ponto flutuante
-bonus = float(input("Digite o bônus recebido: "))
+
+cotniuar_bonus = ""
+
+while cotniuar_bonus != "true":
+    try:
+        bonus = float(input("Digite o bônus recebido: "))
+        cotniuar_bonus = "true"
+    except ValueError:
+        print("Digite um valor válido, um número")
+        exit()
+
 # 4) Calcule o valor do bônus final
 result = 1000 + salario * bonus
+
 # 5) Imprima cálculo do KPI para o usuário
 print(result)
+
 # 6) Imprime a mensagem personalizada incluindo o nome do usuário, salário e bônus
 print(f" {nome} seu salário com o bônus fica: {result}")
 
